ed42819f0e799ad1566b6946907f11fe1394c98a
Onur Yılmaz <csonuryilmaz@gmail.com>
2018-03-04 22:08:06 +0300

    Added location support

--- download/download.sh
+++ download/download.sh
@@ -12,8 +12,12 @@ fi
 
 if [ -z "$SAVE_AS" ]; then
     echo "[*] Auto detecting save as argument ..."
-    SAVE_AS=$(curl -sIL  $URL | grep -o -E 'filename=.*$' | tr -d '\r\n' | sed -e 's/filename=//')
+    SAVE_AS=$(curl -sI  $URL | grep -o -E 'filename=.*$' | tr -d '\r\n' | sed -e 's/filename=//')
 fi
+if [ -z "$SAVE_AS" ]; then
+    SAVE_AS=$(curl -sI  $URL | grep -o -E 'location:.*$' | tr -d '\r\n' | rev | cut -d'/' -f1 | rev)
+fi
+
 echo "[i] Downloaded file will be saved as: $SAVE_AS"
 
 curl -o $SAVE_AS -L $URL
